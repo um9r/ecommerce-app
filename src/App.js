3fa72f902b25ee88022d2297f50cebc6a829635b
Umar <faroukumar99@gmail.com>
2022-11-21 23:55:57 +0100

    library react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import "./index.css";
 import Nav from "./components/Nav";
-import Landing from "./components/Landing";
-import Highlights from "./components/Highlights";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    const dupeItem = cart.find((item) => +item.id === +book.id);
+    if (dupeItem) {
+      setCart(
+        cart.map((item) => {
+          if (item.id === dupeItem.id) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          } else {
+            return item;
+          }
+        })
+      );
+    } else {
+      setCart([...cart, { ...book, quantity: 1 }]);
+    }
+  }
+
+  useEffect(() => {}, [cart]);
+
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => <BookInfo books={books} addToCart={addToCart} />}
+        />
+        <Route path="/cart" render={() => <Cart books={books} />} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/ui/Book.jsx
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
+import Price from "./Price";
+import Rating from "./Rating";
+
+const Book = ({ book }) => {
+
+  return (
+    <div className="book">
+      <Link to={`/books/${book.id}`}>
+        <figure className="book__img--wrapper">
+          <img src={book.url} alt="" className="book__img" />
+        </figure>
+      </Link>
+      <div className="book__title">
+        <Link to={`/books/${book.id}`} className="book__title--link">
+          {book.title}
+        </Link>
+      </div>
+      <Rating rating={book.rating} />
+      <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+    </div>
+  );
+};
+
+export default Book;
